13d4742718229267c7f196e3fb06b064ecce3514
aditya-7000 <adityaayinapurapu18@gmail.com>
2023-07-15 15:02:43 +0530

    initial commit

---
+++ project/backend/server.js
@@ -0,0 +1,216 @@
+
+// require('dotenv').config();
+// const express = require('express');
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+
+// const app = express();
+// const port = 5000;
+
+// // Middleware
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// // Connect to MongoDB
+// mongoose
+//   .connect('mongodb+srv://adi:1234@cluster0.dfglsmv.mongodb.net/?retryWrites=true&w=majority', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log('Connected to MongoDB');
+//   })
+//   .catch((error) => {
+//     console.error('Error connecting to MongoDB:', error);
+//   });
+
+// // User Schema
+// const userSchema = new mongoose.Schema({
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//   },
+//   role: {
+//     type: String,
+//     required: true,
+//   },
+// });
+
+// // User Model
+// const User = mongoose.model('User', userSchema);
+
+// // Routes
+// app.post('/api/register', async (req, res) => {
+//   try {
+//     const { email, password, role } = req.body;
+//     const existingUser = await User.findOne({ email });
+//     if (existingUser) {
+//       return res.status(409).json({ error: 'User already exists' });
+//     }
+
+//     const hashedPassword = await bcrypt.hash(password, 10);
+
+//     const newUser = new User({ email, password: hashedPassword, role });
+//     await newUser.save();
+
+//     res.status(200).json({ message: 'Registration successful' });
+//   } catch (error) {
+//     res.status(500).json({ error: 'Registration failed' });
+//   }
+// });
+
+// // app.post('/api/login', async (req, res) => {
+// //   try {
+// //     const { email, password } = req.body;
+
+// //     const user = await User.findOne({ email });
+// //     if (!user) {
+// //       return res.status(401).json({ error: 'Invalid email or password' });
+// //     }
+
+// //     const isPasswordValid = await bcrypt.compare(password, user.password);
+// //     if (!isPasswordValid) {
+// //       return res.status(401).json({ error: 'Invalid email or password' });
+// //     }
+
+// //     const token = jwt.sign({ email }, process.env.SECRET_KEY);
+
+// //     res.status(200).json({ token });
+// //   } catch (error) {
+// //     res.status(500).json({ error: 'Login failed' });
+// //   }
+// // });
+
+// // Start the server
+
+// app.post('/api/login', async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+
+//     const user = await User.findOne({ email });
+//     if (!user) {
+//       return res.status(401).json({ error: 'Invalid email or password' });
+//     }
+
+//     const isPasswordValid = await bcrypt.compare(password, user.password);
+//     if (!isPasswordValid) {
+//       return res.status(401).json({ error: 'Invalid email or password' });
+//     }
+
+//     const token = jwt.sign({ email }, process.env.SECRET_KEY);
+
+//     // Include the role field in the response
+//     res.status(200).json({ token, role: user.role });
+//   } catch (error) {
+//     res.status(500).json({ error: 'Login failed' });
+//   }
+// });
+
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+require('dotenv').config();
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose
+  .connect('mongodb+srv://adi:1234@cluster0.dfglsmv.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+// User Schema
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+});
+
+// User Model
+const User = mongoose.model('User', userSchema);
+
+// Routes
+app.post('/api/register', async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ error: 'User already exists' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({ email, password: hashedPassword, role });
+    await newUser.save();
+
+    res.status(200).json({ message: 'Registration successful' });
+  } catch (error) {
+    res.status(500).json({ error: 'Registration failed' });
+  }
+});
+
+app.post('/api/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+
+    const token = jwt.sign({ email }, process.env.SECRET_KEY);
+    const role = user.role; // Assuming the role is stored in the "role" field of the user document
+
+    res.status(200).json({ token, role });
+  } catch (error) {
+    res.status(500).json({ error: 'Login failed' });
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
